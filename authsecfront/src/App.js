4256997980997a5cddbd9dcd38bc3539a14483e9
salma <chtiouisalma790@gmail.com>
2025-03-27 20:24:33 +0000

    Ajout du contenu d'authsecfront directement

--- authsecfront/src/App.js
+++ authsecfront/src/App.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import ChangePassword from './components/ChangePassword';
 import Logout from './components/Logout';
+
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('accessToken'));
 
   return (
     <div className="App">
-      <h1>Secure Application</h1>
-      {!token ? (
-        <div>
-          <Register />
-          <Login setToken={setToken} />
-        </div>
-      ) : (
-        <div>
-          <ChangePassword token={token} />
-          <Logout setToken={setToken} />
-        </div>
-      )}
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login setToken={setToken} />} />
+          <Route path="/change-password" element={<ChangePassword token={token} />} />
+          <Route path="/logout" element={<Logout setToken={setToken} />} />
+          
+          {/* You might want to add a default route */}
+         
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
